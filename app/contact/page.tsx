276b55ed235b4525d0aee03f67edbedd1459a60f
Jake Giguere <giguere.jake@gmail.com>
2023-07-26 12:30:05 -0400

    Updated Contacts & Projects

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Twitter, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/jakegiguere/",
+		label: "Linkedin",
+		handle: "Jake Giguere",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:giguere.jake@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "giguere.jake@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/giguerejatwit",
 		label: "Github",
-		handle: "chronark",
+		handle: "giguerejatwit",
 	},
 ];
 
